f498e71cc1a7616d99eb25c7f4ea55ad8cacc0eb
Arpit Jha <arpit.jha@pkware.com>
2022-04-07 14:00:08 +0530

    Initial commit(Includes Registration , Authorization, and Login)

---
+++ src/main/java/com/example/springredditclone/dto/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.example.springredditclone.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class AuthenticationResponse {
+    private String authenticationToken;
+    private String username;
+}
